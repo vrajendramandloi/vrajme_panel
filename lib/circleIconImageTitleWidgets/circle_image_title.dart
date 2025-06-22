e67302a83d782e97324c49254fceaaec59bdf346
vikkymandloi <imvrajendra@gmail.com>
2025-06-23 00:08:43 +0530

    fix for Circle Image title.

--- lib/circleIconImageTitleWidgets/circle_image_title.dart
+++ lib/circleIconImageTitleWidgets/circle_image_title.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-class CircleImageTitle extends StatelessWidget {
-  const CircleImageTitle({
+class CircleImageTitleLocal extends StatelessWidget {
+  const CircleImageTitleLocal({
     super.key,
     this.image,
     this.defaultImage,
@@ -11,11 +11,11 @@ class CircleImageTitle extends StatelessWidget {
     this.backgroundColor,
     this.textColor = Colors.black,
     this.isEnabled = true,
-    this.onCancel,  // Callback for the cancel button
+    this.onCancel, // Callback for the cancel button
     this.cancelButtonSize = 20.0, // Size of the cancel button
   });
 
-  final ImageProvider? image;       // Now accepts an ImageProvider (or null)
+  final ImageProvider? image; // Now accepts an ImageProvider (or null)
   final ImageProvider? defaultImage; //  For default image. Make this nullable.
   final String text;
   final VoidCallback? onPressed;
@@ -23,38 +23,38 @@ class CircleImageTitle extends StatelessWidget {
   final Color? backgroundColor;
   final Color textColor;
   final bool isEnabled;
-  final VoidCallback? onCancel;       // Callback for cancel button
+  final VoidCallback? onCancel; // Callback for cancel button
   final double cancelButtonSize;
-
 
   @override
   Widget build(BuildContext context) {
-        //final height = width * (4 / 3);
+    //final height = width * (4 / 3);
     final height = width * (4 / 3); // Conditional height
     final radius = width / 2;
     final padding = width / 24;
     final textScaleFactor = width / 75.0;
     final cancelButtonSize = radius / 2;
-    
+
     // Calculate colors based on isEnabled
     final calculatedBackgroundColor = isEnabled ? backgroundColor : Colors.grey;
     final calculatedTextColor = isEnabled ? textColor : Colors.grey[400];
 
     // Determine which image to use
-    final imageToDisplay = image ?? defaultImage; // Use default if image is null
+    final imageToDisplay =
+        image ?? defaultImage; // Use default if image is null
 
-    return Stack( // Use a Stack to position the cancel button
+    return Stack(
+      // Use a Stack to position the cancel button
       children: [
         InkWell(
           onTap: isEnabled ? onPressed : null,
-          child: SizedBox(
-            width: width,
-            height: height,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                // Use CircleAvatar to display the image
-                ElevatedButton(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              SizedBox(
+                width: width,
+                height: height,
+                child: ElevatedButton(
                   onPressed: isEnabled ? onPressed : null,
                   style: ElevatedButton.styleFrom(
                     shape: const CircleBorder(),
@@ -64,39 +64,52 @@ class CircleImageTitle extends StatelessWidget {
                   ),
                   // Display image or placeholder
                   child: imageToDisplay != null
-                      ? Stack(  // Use a Stack to overlay the gray avatar
-                        alignment: Alignment.center,
-                        children: [
-                          CircleAvatar( // The original image avatar
-                            radius: radius - padding,
-                            backgroundImage: imageToDisplay,
+                          ? Stack(
+                            // Use a Stack to overlay the gray avatar
+                            alignment: Alignment.center,
+                            children: [
+                              CircleAvatar(
+                                // The original image avatar
+                                radius: radius - padding,
+                                backgroundImage: imageToDisplay,
+                              ),
+                              if (!isEnabled) // Only show gray overlay if disabled
+                                CircleAvatar(
+                                  // Gray overlay avatar
+                                  radius: radius - padding,
+                                  backgroundColor: const Color.fromRGBO(
+                                    158,
+                                    158,
+                                    158,
+                                    0.7,
+                                  ), // Adjust opacity as needed
+                                ),
+                            ],
+                          )
+                          : Icon(
+                            // Default Icon if no image is provided
+                            Icons.image,
+                            size: radius - padding,
+                            color:
+                                Colors
+                                    .grey[600], // Slightly darker grey for placeholder
                           ),
-                          if (!isEnabled) // Only show gray overlay if disabled
-                            CircleAvatar(  // Gray overlay avatar
-                              radius: radius - padding,
-                              backgroundColor: const Color.fromRGBO(158, 158, 158, 0.7), // Adjust opacity as needed
-                            ),
-                        ],
-                      )
-                      : Icon(  // Default Icon if no image is provided
-                          Icons.image,
-                          size: radius - padding,
-                          color: Colors.grey[600], // Slightly darker grey for placeholder
-                        ),
                 ),
-                Padding(
-                  padding: EdgeInsets.only(bottom: padding / 2),
-                  child: Text(
-                    text,
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      color: calculatedTextColor,
-                      fontSize: 14 * textScaleFactor,
-                    ),
+              ),
+              SizedBox(
+                width: width,
+                child: Text(
+                  text,
+                  textAlign: TextAlign.center,
+                  maxLines: 2,
+                  overflow: TextOverflow.ellipsis,
+                  style: TextStyle(
+                    color: calculatedTextColor,
+                    fontSize: 14 * textScaleFactor,
                   ),
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
         if (onCancel != null) // Only show cancel button if onCancel is provided
@@ -112,7 +125,11 @@ class CircleImageTitle extends StatelessWidget {
                 ),
                 width: cancelButtonSize,
                 height: cancelButtonSize,
-                child: Icon(Icons.cancel, size: cancelButtonSize * 0.8, color: Colors.red),
+                child: Icon(
+                  Icons.cancel,
+                  size: cancelButtonSize * 0.8,
+                  color: Colors.red,
+                ),
               ),
             ),
           ),
